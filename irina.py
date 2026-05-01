cdebb19dd87ecbe9337930541d220dfed1dc05d3
Sancho-Pancho-04 <supersaxar2007@gmail.com>
2026-05-01 19:15:51 +0300

    Update4 irina.py

--- irina.py
+++ irina.py
@@ -6,10 +6,12 @@ from datetime import datetime, timedelta
 TOKEN = os.environ.get("8641202785:AAGDzpqE2HsIE6WjO-iA2RL5U2fjDa-O4e8")
 bot = telebot.TeleBot(TOKEN)
 
-CHAT_ID = 123456789  # ВСТАВЬТЕ ВАШ ID
+CHAT_ID = None  # ВСТАВЬТЕ ВАШ ID
 
 @bot.message_handler(commands=['start'])
 def start(message):
+    global CHAT_ID
+    CHAT_ID = message.chat.id
     bot.reply_to(message, "✅ Бот запущен! Таблетос в 21:00")
 
 def send_pill():
